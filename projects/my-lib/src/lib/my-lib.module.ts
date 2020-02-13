632632e303074143358a5432611946d7ef4b9b06
Budahazi Viktor <Viktor.Budahazi@kaleidoscope.at>
2020-02-13 11:31:32 +0100

    Add CommonModule to lib as well

--- projects/my-lib/src/lib/my-lib.module.ts
+++ projects/my-lib/src/lib/my-lib.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MyLibComponent } from './my-lib.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [MyLibComponent],
   imports: [
+    CommonModule,
   ],
   exports: [MyLibComponent]
 })
